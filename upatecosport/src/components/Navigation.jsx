9fec9d7ab84d97d69a7738dd858f10dd8db38fd4
Gonzalo Arce <gonza_arc@hotmail.com>
2024-07-22 22:34:38 -0300

    Se agrego el contenido correspondiente a cada componente y pagina y estilos basicos

--- upatecosport/src/components/Navigation.jsx
+++ upatecosport/src/components/Navigation.jsx
@@ -8,6 +8,7 @@ const Navigation = () => {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
         <li><Link to="/profile">Profile</Link></li>
       </ul>
     </nav>
